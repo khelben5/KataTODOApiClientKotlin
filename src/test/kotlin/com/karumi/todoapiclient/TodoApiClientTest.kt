5801e4e2e0cc69366cb5694b87ef9f7a755bec62
Eduardo Pascua <pascua.eduardo@gmail.com>
2019-01-25 16:46:30 +0100

    Tests for get task by id.

--- src/test/kotlin/com/karumi/todoapiclient/TodoApiClientTest.kt
+++ src/test/kotlin/com/karumi/todoapiclient/TodoApiClientTest.kt
@@ -3,8 +3,11 @@ package com.karumi.todoapiclient
 import org.junit.Assert.*
 import org.junit.Before
 import org.junit.Test
+import todoapiclient.Either
 import todoapiclient.TodoApiClient
 import todoapiclient.dto.TaskDto
+import todoapiclient.exception.ItemNotFoundError
+import todoapiclient.exception.NetworkError
 import todoapiclient.exception.UnknownApiError
 
 class TodoApiClientTest : MockWebServerTest() {
@@ -72,6 +75,106 @@ class TodoApiClientTest : MockWebServerTest() {
 
         assertEquals(0, emptyList.size)
     }
+
+    @Test
+    fun shouldParseCorrectlyWhenGettingTaskById() {
+        enqueueMockResponse(200, "getTaskByIdResponse.json")
+
+        val task = apiClient.getTaskById("1").right!!
+
+        assertEquals(
+            givenAnyTask(),
+            task
+        )
+    }
+
+    @Test
+    fun shouldSendGetTaskByIdToTheCorrectEndPoint() {
+        enqueueMockResponse(200, "getTaskByIdResponse.json")
+
+        apiClient.getTaskById("1")
+
+        assertGetRequestSentTo("/todos/1")
+    }
+
+    @Test
+    fun shouldReturnNotFoundWhenGettingNotExistingTask() {
+        enqueueMockResponse(404)
+
+        val task = apiClient.getTaskById("2").left
+
+        assertEquals(ItemNotFoundError, task)
+    }
+
+    @Test
+    fun shouldReturnErrorIf500WhenGettingTaskById() {
+        enqueueMockResponse(500)
+
+        val error = apiClient.getTaskById("1").left
+
+        assertEquals(UnknownApiError(500), error)
+    }
+
+    @Test
+    fun shouldReturnMalformedJsonWhenGettingTaskById() {
+        enqueueMockResponse(200, "getTaskByIdMalformedResponse.json")
+
+        val result = apiClient.getTaskById("1")
+
+        assertEquals(Either.Left(NetworkError), result)
+    }
+
+    @Test
+    fun shouldCreateTaskCorrectly() {
+        enqueueMockResponse(201, "updateTaskResponse.json")
+
+        val expected = givenAnyTask()
+        val task = apiClient.updateTaskById(expected).right!!
+
+        assertEquals(expected, task)
+    }
+
+    @Test
+    fun shouldReturnBadRequest() {
+        enqueueMockResponse(400)
+
+        val error = apiClient.updateTaskById(givenAnyTask()).left!!
+
+        assertEquals(UnknownApiError(400), error)
+    }
+
+    @Test
+    fun shouldSendCorrectRequestToUpdateTask() {
+        enqueueMockResponse(201, "updateTaskResponse.json")
+
+        apiClient.updateTaskById(
+            TaskDto(
+                id = "1",
+                userId = "2",
+                title = "Finish this kata",
+                isFinished = false
+            )
+        )
+
+        assertRequestBodyEquals("updateTaskRequest.json")
+    }
+
+    @Test
+    fun shouldReturnServerErrorWhenUpdatingTask() {
+        enqueueMockResponse(500)
+
+        val error = apiClient.updateTaskById(givenAnyTask()).left
+
+        assertEquals(UnknownApiError(500), error)
+    }
+
+    private fun givenAnyTask() =
+        TaskDto(
+            id = "1",
+            userId = "1",
+            title = "delectus aut autem",
+            isFinished = false
+        )
 
     private fun assertTaskContainsExpectedValues(task: TaskDto?) {
         assertTrue(task != null)
